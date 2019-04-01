34ca8bc9d4024ab90dc7a1b76ed5a985979afbf8
MR_Infinity <ishubot@gmail.com>
2019-04-01 14:04:51 +0530

    in bobby using redux

--- src/Main.js
+++ src/Main.js
@@ -8,46 +8,27 @@ import AddPhoto from './components/AddPhoto'
 
 class Main extends React.Component
 {
-   constructor(){
+  
 
-        super();
-           this.state={posts:[]};
-           this.removePost=this.removePost.bind(this);
-           this.addPhoto=this.addPhoto.bind(this);
-   }
-   componentDidMount(){
-           const data = Api.getData();
-           console.log(data)
-           this.setState({posts:data});
-   }
-    addPhoto(post){
-            const posts = this.state.posts;
-            posts.unshift(post)
-            console.log(posts)
-        this.setState({posts:posts})
-    }
-    removePost(post){
-      this.setState({posts:this.state.posts.filter((x)=>{return post!==x})})
-    }
    render(){
-       
+       console.log(this.props.posts)
        return(<div>
 
           <Route exact path="/" render={()=>(
                   <div>
                   <Title title={'Photowall'}/>
-               
-                  <Photowall items={this.state.posts} onRemovePost={this.removePost} />
+                
+                   <Photowall items={this.props.posts} onRemovePost={this.removePost} />
                   </div>
           )}/>
-          <Route  path="/addPhoto" render={({history})=>(
+          {/* <Route  path="/addPhoto" render={({history})=>(
                   <div>
                  <AddPhoto onAddPost={(post)=>{
                          this.addPhoto(post);
                             history.push("/");
                  }}/>
                   </div>
-          )}/>
+          )}/> */}
                
                </div>
        );
--- src/index.js
+++ src/index.js
@@ -4,9 +4,12 @@ import Main from './Main';
 import {BrowserRouter} from 'react-router-dom'
 import {createStore} from 'redux';
 import rootReducer from '../src/reducer/reducer'
+import {Provider} from 'react-redux'
+import App from './reducer/App'
 
 const store = createStore(rootReducer);
 
-ReactDOM.render(<BrowserRouter><Main /></BrowserRouter>, document.getElementById('root'));
+
+ReactDOM.render(<Provider store={store}><BrowserRouter><App /></BrowserRouter></Provider>, document.getElementById('root'));
  
 
